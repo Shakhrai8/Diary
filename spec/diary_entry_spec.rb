5e4efd2881eb0a5b2f452fe655691e277f3e9b98
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-05-16 22:20:45 +0100

    add diary_entry test unit, passes tests

--- spec/diary_entry_spec.rb
+++ spec/diary_entry_spec.rb
@@ -1,12 +1,21 @@
-#context "#reading_time method" do
-    #it "returns an estimate amount of time neede to read in minutes for a given text" do
-      #diary.add(entry_1)
-      #expect(diary.reading_time(1)).to eq 5
-    #end
+require 'diary_entry'
 
-    #it "returns 2" do
-      #diary.add(entry_1)
-      #diary.add(entry_2)
-      #expect(diary.reading_time(5)).to eq 2
-    #end
-  #end
+RSpec.describe DiaryEntry do
+  let(:entry) { DiaryEntry.new("2023-05-01", "Today was a great day!") }
+
+  context "#count_words method" do
+    it "returns the correct number of words in the content" do
+      expect(entry.count_words).to eq 5
+    end
+  end
+
+  context "#reading_time method" do
+    it "calculates the reading time correctly for a given words per minute (wpm)" do
+      expect(entry.reading_time(100)).to eq 1
+    end
+
+    it "raises an error for a negative value of wpm" do
+      expect { entry.reading_time(-50) }.to raise_error(RuntimeError, "Error: wpm should be a non-negative integer")
+    end
+  end
+end
